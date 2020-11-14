8515b99a21a2d8353dd6d1ecb563f0cc46876895
lyx <765020742@qq.com>
2020-11-14 12:19:18 +0800

    新增前端错误状态码

--- cyber_security/util/httpTools.py
+++ cyber_security/util/httpTools.py
@@ -5,10 +5,11 @@
 from django.http import JsonResponse
 
 class RestResponse:
-    SUCCESS = 200
-    AUTH_ERROR = 401
-    USER_ERROR = 402
-    SERVER_ERROR = 500
+    SUCCESS = 200  # 成功
+    AUTH_ERROR = 401  # 认证错误
+    USER_ERROR = 402  # 用户操作错误
+    FRONT_ERROR = 411  # 前端操作错误
+    SERVER_ERROR = 500  # 服务端错误
 
     @staticmethod  # 封装json接口
     def success(msg='', data={}):
@@ -27,5 +28,9 @@ class RestResponse:
         return JsonResponse({'code': RestResponse.USER_ERROR, 'msg': msg, 'data': data})
 
     @staticmethod
+    def frontFail(msg='', data={}):
+        return JsonResponse({'code': RestResponse.FRONT_ERROR, 'msg': msg, 'data': data})
+
+    @staticmethod
     def serverFail():
-        return JsonResponse({'code': RestResponse.SERVER_ERROR, 'msg': '服务器错误！', 'data': None})
+        return JsonResponse({'code': RestResponse.SERVER_ERROR, 'msg': '服务器出错！', 'data': None})
